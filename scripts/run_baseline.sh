af817bf8229c144e4aa23b0e534fddeff214abf7
DedSecer <52042233+DedSecer@users.noreply.github.com>
2024-01-28 09:41:15 +0800

    using vscode-server in baseline

--- scripts/run_baseline.sh
+++ scripts/run_baseline.sh
@@ -6,6 +6,7 @@ sudo docker run -it --name baseline --network=host \
     --device=/dev/ttyUSB0 \
     --device=/dev/input/js0 \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v ~/vscode-server:/root/.vscode-server \
     -e DISPLAY=$DISPLAY \
     -v $HOME/Desktop/shared:/shared \
     jieyitsinghuawx/icra2024-sim2real-axs-baseline:v1.0.0 \
